32ab9d8a1ff4e0024df1741127040aa1f26c8e86
dingle <18911755085@163.com>
2020-03-13 00:51:11 +0800

    初始化activity

---
+++ event_rn/android/app/src/main/java/com/event_rn/MyLanJinagTouchActivity.java
@@ -0,0 +1,15 @@
+package com.event_rn;
+
+import android.os.Bundle;
+
+import com.example.nopermisstionad_sdk.LanJinagTouchActivity;
+
+public class MyLanJinagTouchActivity extends LanJinagTouchActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}

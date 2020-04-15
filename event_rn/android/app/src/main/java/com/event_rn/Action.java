abd5145944ffa23fec4df6070d452abe90045faf
dingle <18911755085@163.com>
2020-04-15 18:34:56 +0800

    完成启动图倒计时

---
+++ event_rn/android/app/src/main/java/com/event_rn/Action.java
@@ -0,0 +1,9 @@
+package com.event_rn;
+
+/**
+ * Created by linlongxin on 2016/8/29.
+ */
+
+public interface Action {
+    void onAction();
+}
--- event_rn/android/app/src/main/java/com/event_rn/Splash.java
+++ event_rn/android/app/src/main/java/com/event_rn/Splash.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 /*
 * 启动页
@@ -12,28 +14,52 @@ import android.view.WindowManager;
 * */
 public class Splash extends Activity {
 
+    CountdownView mCountdownView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);//隐藏状态栏
 
 //        getActionBar().hide();//隐藏标题栏
-        setContentView(R.layout.activity_splash);
-        Thread myThread=new Thread(){//创建子线程
-            @Override
-            public void run() {
-                try{
-                    sleep(1000);//使程序休眠1秒
-                    Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
-                    startActivity(it);
-                    finish();//关闭当前活动
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        };
-        myThread.start();//启动线程
+//        Thread myThread=new Thread(){//创建子线程
+//            @Override
+//            public void run() {
+//                try{
+//                    sleep(30000);//使程序休眠1秒
+//                    Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
+//                    startActivity(it);
+//                    finish();//关闭当前活动
+//                }catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        };
+//        myThread.start();//启动线程
         setContentView(R.layout.activity_splash);
 
+
+        mCountdownView = (CountdownView) findViewById(R.id.countdown);
+        mCountdownView.start();
+        mCountdownView.setOnFinishAction(new Action() {
+            @Override
+            public void onAction() {
+
+                Splash.this.gotoMain();
+            }
+        });
+        mCountdownView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Splash.this.gotoMain();
+            }
+        });
+
+    }
+
+    public void gotoMain(){
+        Intent it=new Intent(getApplicationContext(),MainActivity.class);//启动MainActivity
+        startActivity(it);
+        finish();//关闭当前活动
     }
 }
